29c73219d4c2f5178191237211f9067c2d77fcea
Zicong Mei <zicong@google.com>
2024-10-11 14:49:17 -0700

    add chatbot

---
+++ chatbot.py
@@ -0,0 +1,19 @@
+import vertexai
+
+from vertexai.generative_models import GenerativeModel, ChatSession
+
+PROJECT_ID = "zicong-gke-multi-cloud-dev-2"
+vertexai.init(project=PROJECT_ID, location="us-central1")
+
+model = GenerativeModel("gemini-1.5-flash-002")
+
+chat_session = model.start_chat()
+
+def get_chat_response(chat: ChatSession, prompt: str) -> str:
+    response = chat.send_message(prompt)
+    return response.text
+
+while True:
+    # Get the input data from the user
+    prompt = input("> ")
+    print(get_chat_response(chat_session, prompt))
